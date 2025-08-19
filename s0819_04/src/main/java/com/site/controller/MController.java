bb69cc2f178dda8ae071a4d49e7a1f7155c162eb
minkim-korea <km24778@gmail.com>
2025-08-19 18:21:08 +0900

    views,controller,service 구현

--- s0819_04/src/main/java/com/site/controller/MController.java
+++ s0819_04/src/main/java/com/site/controller/MController.java
@@ -1,7 +1,9 @@
 package com.site.controller;
 
 import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,22 +11,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.site.dto.Member;
+import com.site.service.MService;
+import com.site.service.MServiceImpl;
+import com.site.service.MServiceImpl2;
 
 @Controller
 public class MController {
 
+	@Autowired MService mService;
+	
+	@GetMapping("/member/mList")
+	public String mList(Model model) {
+		
+		List<Member> list = mService.selectAll();
+		model.addAttribute("list",list);
+		return "member/mList";
+	}
+	
+	@GetMapping("/member/mView")
+	public String mView(Model model) {
+		// member 정보
+//		MServiceImpl mServiceImpl = new MServiceImpl();
+//		Member member = mServiceImpl.selectOne();
+		// 유관순 정보를 가져와서 member전송하시오.
+//		MService mService = new MServiceImpl2();
+//		Member member = mService.selectOne();
+		
+		//스프링이 객체선언을 대행해서 주입(DI)
+		Member member = mService.selectOne();
+		model.addAttribute("member",member);
+		return "member/mView";
+	}
+	
 	@GetMapping("/member/member")
 	public String member() {
 		return "member/memberInput";
 	}
 	
 	@PostMapping("/member/member")
-	public String member(Member member,
-			@RequestParam("hobby") String[] hobby,
-			Model model) {
-		
-		System.out.println(Arrays.toString(hobby));
-		model.addAttribute("member",member);
-		return "member/memberResult";
+	public String member(Member member,Model model) {
+
+		mService.insertOne(member);
+		return "redirect:/member/mList";
 	}
 }
